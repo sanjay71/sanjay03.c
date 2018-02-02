f85ee20025abb711609a408f7c4055fdbb55cb15
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-02-02 13:38:47 +0530

    Create anand04.c

---
+++ anand04.c
@@ -0,0 +1,14 @@
+#include<stido.h>
+#include<conio.h>
+int main()
+{
+char b;
+printf("enter the letters");
+scanf("%c",&b);
+if(b=='a' !! b=='e' !! b=='i' !! b=='o' !! b=='u')
+printf("it is the vowel letter");
+else
+printf("it is not the vowel letter");
+getch();
+return 0;
+}
