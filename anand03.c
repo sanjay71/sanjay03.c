c1fb5a008647579ebcb4992065bfc3ae4aac5c5a
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-02-02 13:22:57 +0530

    n = int(input()) if n<0 :     print ("Negative") elif n>0:     print ("Positive") else:     print("Zero") © 2018 GitHub, Inc.

---
+++ anand03.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+n = int(input()
+clrscr();
+if n<0 :
+    print ("Negative")
+elif n>0;
+    print ("Positive")
+else:
+    print("Zero")
+    getch();
+    return0;
+    }
