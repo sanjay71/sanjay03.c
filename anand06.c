7bd2a46800e7ec18e31d257e8092106e8919ca5c
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-02-02 14:05:25 +0530

    Create anand06.c

---
+++ anand06.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+clrscr();
+printf("enter three number");
+scanf("%d%d%d",&a&b&c);
+if((a>b)&&( a>c)
+{
+printf("a is greater");
+}
+else((b>c)&&(b>a)
+{
+printf("b is greater");
+}
+else(c>a)&&(c>b);
+{printf("c is greater");
+}
+getch();
+return0;
+}
