b8ba6bc18270910acd5459ca03cb0ea7096deb32
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-02-02 13:52:22 +0530

    Create anand05.c

---
+++ anand05.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char b;
+printf("enter the letter");
+scanf("%c",&b);
+if
+b=a;
+printf("it is a alphabet");
+else
+printf("it is not a alphabet");
+getch();
+return 0;
+}
